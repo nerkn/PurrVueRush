b42c0504b8c7a1908ceaaf685cd445c0267cc363
Erkin Tek <erkintek@gmail.com>
2024-01-08 01:34:31 +0300

    listen

--- fast/server.ts
+++ fast/server.ts
@@ -25,7 +25,7 @@ fastify.register(productsRoute);
 fastify.register(fileUploadRoute);
 
 // Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ host: "0.0.0.0", port: 3000 }, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
